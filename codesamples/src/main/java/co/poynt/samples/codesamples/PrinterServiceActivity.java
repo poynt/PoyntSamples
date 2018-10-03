fa5e697843857b9a8471eb07fc38dc2df96d595d
deepak <deepak@poynt.co>
2018-10-02 18:25:05 -0700

    Moved printer service to helper class with support for unbinding TODO need to improve the code

--- codesamples/src/main/java/co/poynt/samples/codesamples/PrinterServiceActivity.java
+++ codesamples/src/main/java/co/poynt/samples/codesamples/PrinterServiceActivity.java
@@ -8,12 +8,15 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -21,12 +24,16 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import co.poynt.os.model.AccessoryProvider;
+import co.poynt.os.model.PrintedReceipt;
+import co.poynt.os.model.PrintedReceiptLine;
+import co.poynt.os.model.PrintedReceiptLineFont;
+import co.poynt.os.model.PrintedReceiptSection;
+import co.poynt.os.model.PrintedReceiptV2;
 import co.poynt.os.model.PrinterStatus;
 import co.poynt.os.services.v1.IPoyntPrinterService;
+import co.poynt.samples.codesamples.utils.PrinterServiceHelper;
 
-import static co.poynt.os.services.v1.IPoyntPrinterServiceListener.*;
-
-public class PrinterServiceActivity extends Activity {
+public class PrinterServiceActivity extends Activity{
 
     public static String TAG = PrinterServiceActivity.class.getSimpleName();
     private PrinterServiceHelper printerServiceHelper;
@@ -48,16 +55,19 @@ public class PrinterServiceActivity extends Activity {
         setContentView(R.layout.activity_printer_service);
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-        ButterKnife.bind(this);
 
-        printerServiceHelper = new PrinterServiceHelper(this);
+        ButterKnife.bind(this);
+        printerServiceHelper = new PrinterServiceHelper(this, printerCallback);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
+                break;
+            case R.id.action_refresh:
+                printerServiceHelper.refreshPrinters();
                 break;
             default:
                 break;
@@ -65,81 +75,61 @@ public class PrinterServiceActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_printer_service, menu);
+        return true;
+    }
+
     @OnClick(R.id.printJobBtn)
-    public void printJob(View view){
-        for (IBinder binder: printerServiceHelper.getPrinters().values()){
-            final IPoyntPrinterService printerService = IPoyntPrinterService.Stub.asInterface(binder);
-            if (printerService!=null){
-                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.poynt_logo);
-                try {
-                    printerService.printJob(UUID.randomUUID().toString(), bitmap,
-                            new Stub() {
-                        @Override
-                        public void onPrintResponse(final PrinterStatus printerStatus, final String s) throws RemoteException {
-                            Log.d(TAG, "printJob print_status  " + printerStatus.getMessage());
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    printerStatusText.append("PrintJob Status: "+ printerStatus.getMessage() +  "\n");
-                                }
-                            });
-                        }
-                    });
-                } catch (RemoteException e) {
-                    e.printStackTrace();
+    public void printJob(View view) {
+        HashMap<AccessoryProvider, IBinder> printers = printerServiceHelper.getPrinters();
+        for (AccessoryProvider printer: printers.keySet()) {
+            if (printer.isConnected()) {
+                final IPoyntPrinterService printerService = IPoyntPrinterService.Stub.asInterface(printers.get(printer));
+                if (printerService != null) {
+                    Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.poynt_logo);
+                    try {
+                        printerService.printJob(UUID.randomUUID().toString(), bitmap, printerServiceHelper.printerServiceListener);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
                 }
+            } else {
+                printerStatusText.append(printer.getProviderName() + ": " + String.valueOf(printer.isConnected()));
+                printerServiceHelper.reconnectPrinter(printer);
             }
         }
     }
+
     // Works only on internal poynt printer
     @OnClick(R.id.printReceiptBtn)
-    public void printReceiptBtn(View view){
+    public void printReceiptBtn(View view) {
         mPrinterServices = printerServiceHelper.getPrinters();
-        for (AccessoryProvider printer: mPrinterServices.keySet()){
+        for (final AccessoryProvider printer : mPrinterServices.keySet()) {
             Log.d(TAG, printer.getProviderName());
-                if(printer.getProviderName().equals("Poynt Printer")){
-                    IPoyntPrinterService printerService = IPoyntPrinterService.Stub.asInterface(mPrinterServices.get(printer));
-                    if (printerService!=null){
-                        try {
-                            printerService.printReceipt(UUID.randomUUID().toString(), PrinterServiceHelper.generateReceiptv2(), new Stub() {
-                                @Override
-                                public void onPrintResponse(final PrinterStatus printerStatus, String s) throws RemoteException {
-                                    Log.d(TAG, "printReceipt print_status  " + printerStatus.getMessage());
-                                    runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            printerStatusText.append("printReceipt Status: "+ printerStatus.getMessage() + "\n");
-                                        }
-                                    });
-                                }
-                            }, null);
-                        } catch (RemoteException e) {
-                            e.printStackTrace();
-                        }
+            if (printer.getProviderName().equals("Poynt Printer")) {
+                IPoyntPrinterService printerService = IPoyntPrinterService.Stub.asInterface(mPrinterServices.get(printer));
+                if (printerService != null) {
+                    try {
+                        printerService.printReceipt(UUID.randomUUID().toString(),
+                                generateReceiptv2(),printerServiceHelper.printerServiceListener, null);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
                     }
                 }
+            }
         }
     }
 
     @OnClick(R.id.printReceiptJobBtn)
-    public void printReceiptJobBtn(View view){
-        for (IBinder binder: printerServiceHelper.getPrinters().values()){
+    public void printReceiptJobBtn(View view) {
+        for (IBinder binder : printerServiceHelper.getPrinters().values()) {
             IPoyntPrinterService printerService = IPoyntPrinterService.Stub.asInterface(binder);
-            if (printerService!=null){
+            if (printerService != null) {
                 try {
-                    printerService.printReceiptJob(UUID.randomUUID().toString(), PrinterServiceHelper.generateReceiptv1(),
-                            new Stub() {
-                                @Override
-                                public void onPrintResponse(final PrinterStatus printerStatus, String s) throws RemoteException {
-                                    Log.d(TAG, "printReceiptJob print_status  " + printerStatus.getMessage());
-                                    runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            printerStatusText.append("printReceiptJob Status: "+ printerStatus.getMessage() + "\n");
-                                        }
-                                    });
-                                }
-                            });
+                    printerService.printReceiptJob(UUID.randomUUID().toString(),
+                            generateReceiptv1(),printerServiceHelper.printerServiceListener);
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
@@ -147,12 +137,140 @@ public class PrinterServiceActivity extends Activity {
         }
     }
 
+    PrinterServiceHelper.PrinterCallback printerCallback = new PrinterServiceHelper.PrinterCallback() {
+        @Override
+        public void onPrinterResponse(final PrinterStatus status) {
+            if(status.getCode().equals(PrinterStatus.Code.PRINTER_DISCONNECTED)){
+                printerStatusText.append("Printer disconnected");
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    printerStatusText.append(status.getMessage()+"\n");
+                }
+            });
+        }
 
+        @Override
+        public void onPrinterReconnect(IBinder binder) {
+        }
+
+        @Override
+        public void logMessage(final String s) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    printerStatusText.append(s +"\n");
+                }
+            });
+        }
+    };
+
+    public static PrintedReceipt generateReceiptv1() {
+        PrintedReceipt printedReceipt = new PrintedReceipt();
+
+        // BODY
+        List<PrintedReceiptLine> body = new ArrayList<PrintedReceiptLine>();
+
+        body.add(newLine(" Check-in REWARD  "));
+        body.add(newLine(""));
+        body.add(newLine("FREE Reg. 1/2 Order"));
+        body.add(newLine("Nachos or CHEESE"));
+        body.add(newLine("Quesadilla with min."));
+        body.add(newLine("$ 15 bill."));
+        body.add(newLine(".................."));
+        body.add(newLine("John Doe"));
+        body.add(newLine("BD: May-5, AN: Aug-4"));
+        body.add(newLine("john.doe@gmail.com"));
+        body.add(newLine("Visit #23"));
+        body.add(newLine("Member since: 15 June 2013"));
+        body.add(newLine(".................."));
+        body.add(newLine("Apr-5-2013 3:25 PM"));
+        body.add(newLine("Casa Orozco, Dublin, CA"));
+        body.add(newLine(".................."));
+        body.add(newLine("John Doe"));
+        body.add(newLine("BD: May-5, AN: Aug-4"));
+        body.add(newLine("john.doe@gmail.com"));
+        body.add(newLine("Visit #23"));
+        body.add(newLine("Member since: 15 June 2013"));
+        body.add(newLine(".................."));
+        body.add(newLine("Apr-5-2013 3:25 PM"));
+        body.add(newLine("Casa Orozco, Dublin, CA"));
+        body.add(newLine(".................."));
+        body.add(newLine("Coupon#: 1234-5678"));
+        body.add(newLine("  Powered by Poynt"));
+        printedReceipt.setBody(body);
+
+        // to print image
+//        printedReceipt.setHeaderImage(BitmapFactory.decodeResource(getResources(), R.drawable.poynt_logo));
+//        printedReceipt.setFooterImage(BitmapFactory.decodeResource(getResources(), R.drawable.poynt_logo));
+
+        return printedReceipt;
+    }
+
+    public static PrintedReceiptLine newLine(String s) {
+        PrintedReceiptLine line = new PrintedReceiptLine();
+        line.setText(s);
+        return line;
+    }
+
+
+    public static PrintedReceiptV2 generateReceiptv2() {
+        PrintedReceiptV2 printedReceipt = new PrintedReceiptV2();
+
+        // Section
+        List<PrintedReceiptLine> body = new ArrayList<PrintedReceiptLine>();
+
+        body.add(newLine(" Check-in REWARD  "));
+        body.add(newLine(""));
+        body.add(newLine("FREE Reg. 1/2 Order"));
+        body.add(newLine("Nachos or CHEESE"));
+        body.add(newLine("Quesadilla with min."));
+        body.add(newLine("$ 15 bill."));
+        body.add(newLine(".................."));
+        body.add(newLine("John Doe"));
+        body.add(newLine("BD: May-5, AN: Aug-4"));
+        body.add(newLine("john.doe@gmail.com"));
+        body.add(newLine("Visit #23"));
+        body.add(newLine("Member since: 15 June 2013"));
+        body.add(newLine(".................."));
+        body.add(newLine("Apr-5-2013 3:25 PM"));
+        body.add(newLine("Casa Orozco, Dublin, CA"));
+        body.add(newLine(".................."));
+        body.add(newLine("John Doe"));
+        body.add(newLine("BD: May-5, AN: Aug-4"));
+        body.add(newLine("john.doe@gmail.com"));
+        body.add(newLine("Visit #23"));
+        body.add(newLine("Member since: 15 June 2013"));
+        body.add(newLine(".................."));
+        body.add(newLine("Apr-5-2013 3:25 PM"));
+        body.add(newLine("Casa Orozco, Dublin, CA"));
+        body.add(newLine(".................."));
+        body.add(newLine("Coupon#: 1234-5678"));
+        body.add(newLine("  Powered by Poynt"));
+
+        // Set Font
+        PrintedReceiptLineFont font = new PrintedReceiptLineFont(PrintedReceiptLineFont.FONT_SIZE.FONT17, 22);
+        PrintedReceiptSection bodySection = new PrintedReceiptSection(body, font);
+
+        printedReceipt.setBody1(bodySection);
+        // to print image
+//        printedReceipt.setHeaderImage(BitmapFactory.decodeResource(getResources(), R.drawable.poynt_logo));
+//        printedReceipt.setFooterImage(BitmapFactory.decodeResource(getResources(), R.drawable.poynt_logo));
+
+        return printedReceipt;
+    }
     public void onResume() {
         super.onResume();
+        //Bind to accessory manager which then binds to all printers
+        printerServiceHelper.bindAccessoryManager();
+
     }
 
-    public void onPause(){
+    public void onPause() {
         super.onPause();
+        printerServiceHelper.unBindServices();
     }
+
+
 }
