561ebec5c64d52ff8487ddb2891f6dfba37cabf9
Kastureema <ksarma@godaddy.com>
2024-03-21 14:19:25 -0700

    DV-51: Update dependencies

--- codesamples/src/main/java/co/poynt/samples/codesamples/CameraActivity.java
+++ codesamples/src/main/java/co/poynt/samples/codesamples/CameraActivity.java
@@ -9,7 +9,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
+import androidx.core.app.ActivityCompat;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.Surface;
